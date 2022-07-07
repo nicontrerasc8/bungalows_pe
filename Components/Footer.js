b72d22f8539eedf99d02477ee634843603b404d3
nicontrerasc8 <nicontrerasc8@gmail.com>
2022-07-07 18:18:12 -0500

    xd

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,17 +1,25 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Image from 'next/image'
 import React from 'react'
-import IG from "../public/ig.svg"
+import IG from "../public/instagram.svg"
 import FB from "../public/fb.svg"
+import YT from "../public/youtube.svg"
+import LI from "../public/linkedin.svg"
 
 const Footer = () => {
   return <footer>
        <h3>Síguenos en: </h3>
        <a href='https://www.instagram.com/bungalows_peru/' target='_blank' rel='noreferrer'>
-          <Image src={IG} height={80} width={80}/>
+          <Image src={IG} height={60} width={60}/>
        </a>
        <a href='https://www.facebook.com/bungalowsPE' target='_blank' rel='noreferrer'>
           <Image src={FB} height={70} width={70}/>
+       </a>
+       <a href='https://www.youtube.com/channel/UCyORLTkS5hJmMQleHhW4xug' target='_blank' rel='noreferrer'>
+          <Image src={YT} height={70} width={70}/>
+       </a>
+       <a href='https://www.linkedin.com/company/bungalows-peru/' target='_blank' rel='noreferrer'>
+          <Image src={LI} height={60} width={60}/>
        </a>
   </footer>
 }
--- Components/WhatsAppButton.js
+++ Components/WhatsAppButton.js
@@ -1,12 +1,26 @@
 import WhatsAppImage from "../public/wa.svg"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from "next/image"
 
 const WhatsAppButton = () => {
 
+    const [Size, setSize] = useState(80)
+
+    const ResizeBtn = () => {
+        if(window.innerWidth > window.innerHeight)
+        setSize(window.innerWidth/18)
+        else setSize(window.innerHeight/16)
+    }
+
+    useEffect(() => {
+      window.addEventListener("resize", ResizeBtn)
+      return 0
+    }, [])
+    
+
 
     return <a rel="noreferrer" target="_blank" className='wa-button' href={'https://wa.me/51998855069'} >
-            <Image src={WhatsAppImage} width={80} height={80}/>
+            <Image src={WhatsAppImage} width={Size} height={Size}/>
         </a>
 }
 
