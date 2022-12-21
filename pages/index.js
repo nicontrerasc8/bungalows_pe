e3fb5d57a71a4672cc9052f82918bf83954fd0a6
Nicolas <nicontrerasc8@gmail.com>
2022-12-21 14:40:56 -0500

    xd

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ const WhyGrid = () => {
 const SVG = () => {
   return <div className="custom-shape-divider-bottom-1639756025">
     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" fill='#ececec'/>
+        <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" fill='#ececec'/> si
     </svg>
   </div>
 }
