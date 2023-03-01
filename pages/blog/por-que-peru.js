34ec1100265b1fdb138f76c59629d3e0bcc6c7cf
Nicolas <nicontrerasc8@gmail.com>
2023-03-01 12:50:46 -0500

    si

--- pages/blog/por-que-peru.js
+++ pages/blog/por-que-peru.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Carrusel from '../../Components/Carrusel'
-import { WhyPeru } from '../../lib/arrays'
+import { BlogArr, WhyPeru } from '../../lib/arrays'
 import {motion} from "framer-motion"
 import MetaTags from '../../Components/MetaTags'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const PorquePeru = () => {
+
+
+  const [OtherArticles, setOtherArticles] = useState([])
+  const router = useRouter()
+
+    useEffect(() => {
+      var arr = []
+      for (let i = 0; i < 3; i++) {
+        var Article 
+        do {
+          var is = false
+          Article = BlogArr[Math.floor(Math.random() * BlogArr.length)]
+          is = (Article.link === "por-que-peru" || arr.includes(Article))
+        } while (is);
+        arr.push(Article)
+      }
+      setOtherArticles(arr)
+    }, [Array, ])
+
   return <>
   <MetaTags title="¿Por qué elegir al Perú como destino turístico?"/>
   <div className='choose-peru'>
@@ -29,6 +50,30 @@ viewport={{ once: true }}
     })
   }
 </div>
+<div className="other-articles">
+      <h4>También te puede interesar</h4>
+      <section>
+  {
+      OtherArticles.length && OtherArticles.map((info,idx) => {
+          return <motion.article 
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          key={idx}>
+            <img src={info.img}/>
+            <div>
+              <h5>{info.title}</h5>
+              <Link href={`/blog/${info.link}`}>
+                <button className='btn-primary'>
+                  Leer artículo
+                </button>
+              </Link>
+            </div>
+          </motion.article>
+      })
+  }
+  </section>
+    </div>
 </>
 }
 
