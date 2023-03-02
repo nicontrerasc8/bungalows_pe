91f9428b4fd624aed45eb1ea5271f019020002ca
Nicolas <nicontrerasc8@gmail.com>
2023-03-02 13:53:23 -0500

    si

--- pages/blog/selvamonos.js
+++ pages/blog/selvamonos.js
@@ -33,9 +33,10 @@ const OxaHaus = () => {
         <h1>Selvámonos, el festival de música de Oxapampa.</h1>
         <span>Escrito por: Cristóbal Espejo</span>
         <img src={"/selvamonos.jpg"} alt="Bungalows Perú"/>
-        <p>Selvámonos es un evento que se realiza entre el 30 de junio y 1ro de julio de cada año. Este festival invita a personas de todo el Perú a conocer Oxapampa y disfrutar de un concierto musical con los mejores artistas.</p>
+        <p>Selvámonos es un evento que se realiza entre el 30 de junio y 1ro de julio de cada año. Este festival invita a personas de todo el Perú a conocer Oxapampa y disfrutar de un concierto musical espectacular. Selvámonos reúne a algunos de los artistas más talentosos de Perú y del mundo, para ofrecer una experiencia inolvidable a los asistentes.</p>
         <h3>Un evento diferente</h3>
-        <p>Este festival es muy especial, ya que aquellos que no residen en Oxapampa pueden acampar cerca del concierto. Muchas empresas turísticas ofrecen paquetes para Selvámonos que incluyen tours por lugares atractivos de Oxapampa, como la Catarata del Río Tigre o el Parque Nacional Yanachaga - Chemillén. </p>
+       <p>La edición más reciente de Selvámonos contó con una gran variedad de géneros musicales, desde la música electrónica y el rock, hasta el reggae y la cumbia. Además, el festival ofrece mucho más que sólo música: también hay talleres de arte y cultura, proyecciones de cine y una amplia selección de comida y bebida.</p>
+       <p>Pero Selvámonos no es sólo un festival de música, sino también una oportunidad para explorar la hermosa ciudad de Oxapampa y sus alrededores. La región es conocida por su rica biodiversidad, y hay muchas opciones para hacer turismo ecológico y de aventura. Desde senderismo y ciclismo de montaña, hasta excursiones a cascadas y ríos.</p>
         <a href='https://selvamonos.org/2023/' target={"_blank"} rel="noreferrer" className='oxahaus-btn'>
             <button className='btn-primary'>
                 <FontAwesomeIcon icon={faInfoCircle}/> Conoce más del festival
