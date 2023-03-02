8155150926d1728586dc9f9eb8f14449795c2d97
Nicolas <nicontrerasc8@gmail.com>
2023-03-02 10:28:19 -0500

    si

--- pages/blog/selvamonos.js
+++ pages/blog/selvamonos.js
@@ -6,7 +6,7 @@ import MetaTags from '../../Components/MetaTags'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStore } from '@fortawesome/free-solid-svg-icons'
+import { faInfo, faInfoCircle, faStore } from '@fortawesome/free-solid-svg-icons'
 
 const OxaHaus = () => {
 
@@ -20,7 +20,7 @@ const OxaHaus = () => {
         do {
           var is = false
           Article = BlogArr[Math.floor(Math.random() * BlogArr.length)]
-          is = (Article.link === "oxahaus" || arr.includes(Article))
+          is = (Article.link === "selvamonos" || arr.includes(Article))
         } while (is);
         arr.push(Article)
       }
@@ -28,26 +28,21 @@ const OxaHaus = () => {
     }, [Array, ])
 
   return <>
-  <MetaTags title="OxaHaus, un rincón de Oxapampa en Lima"/>
+  <MetaTags title="Selvámonos, el festival de música de Oxapampa."/>
   <div className='blog-article top7rem'>
-        <h1>OxaHaus, un rincón de Oxapampa en Lima</h1>
+        <h1>Selvámonos, el festival de música de Oxapampa.</h1>
         <span>Escrito por: Cristóbal Espejo</span>
-        <img src={"/oxahaus.jpeg"} alt="Bungalows Perú"/>
-        <p>OxaHaus es una tienda de productos traídos de Oxapampa en Lima. En las calles de la Molina, resalta mucho por su diseño, que tiene la arquitectura de la cultura austro alemana. Apenas entres a la tienda, sentirás que has viajado al mismo Oxapampa.</p>
-        <div className='imgs-blog'>
-        <img src={"/oxahaus-2.jpg"} alt="Bungalows Perú"/>
-        <img src={"/oxahaus-3.jpg"} alt="Bungalows Perú"/>
-        <img src={"/oxahaus-4.jpg"} alt="Bungalows Perú"/>
-        </div>
-        <h3>Productos de calidad</h3>
-        <p>Aquí podrás conseguir las mejores carnes premium, chorizos, cecina, cerdo y cordero. También productos naturales traídos de Oxapampa, como miel, polen, café, quesos, yogures, leche natural, chocolate y cervezas artesanales. </p>
-        <a href='https://www.oxahaus.com/' target={"_blank"} rel="noreferrer" className='oxahaus-btn'>
+        <img src={"/selvamonos.jpg"} alt="Bungalows Perú"/>
+        <p>Selvámonos es un evento que se realiza entre el 30 de junio y 1ro de julio de cada año. Este festival invita a personas de todo el Perú a conocer Oxapampa y disfrutar de un concierto musical con los mejores artistas.</p>
+        <h3>Un evento diferente</h3>
+        <p>Este festival es muy especial, ya que aquellos que no residen en Oxapampa pueden acampar cerca del concierto. Muchas empresas turísticas ofrecen paquetes para Selvámonos que incluyen tours por lugares atractivos de Oxapampa, como la Catarata del Río Tigre o el Parque Nacional Yanachaga - Chemillén. </p>
+        <a href='https://selvamonos.org/2023/' target={"_blank"} rel="noreferrer" className='oxahaus-btn'>
             <button className='btn-primary'>
-                <FontAwesomeIcon icon={faStore}/> Visitar la página web
+                <FontAwesomeIcon icon={faInfoCircle}/> Conoce más del festival
             </button>
         </a>
-        <p>La tienda está ubicada en Jirón Samoa 382, La Molina.</p>
-        <iframe style={{marginTop: "2vh + 10px"}} src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.886337062216!2d-77.00119678518675!3d-12.119928791418575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c7fd1ffc0dff%3A0x1993b73acb9737cd!2sOxaHaus!5e0!3m2!1ses-419!2spe!4v1677703545935!5m2!1ses-419!2spe'/>
+        <p>Aquí te dejamos un vídeo de Selvámonos. ¡Anímate a ir este año!</p>
+        <iframe style={{marginTop: "2vh + 10px"}} src='https://www.youtube.com/embed/CflGvRXN8Q0'/>
      </div>
 <div className="other-articles">
       <h4>También te puede interesar</h4>
