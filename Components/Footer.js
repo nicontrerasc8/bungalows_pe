0466746099e0dcdd3b0dbd04178df853d9418511
Nicolas <nicontrerasc8@gmail.com>
2023-03-17 13:18:59 -0500

    si

--- Components/Footer.js
+++ Components/Footer.js
@@ -33,13 +33,6 @@ const Footer = () => {
        <a href='https://www.linkedin.com/company/bungalows-peru/' target='_blank' rel='noreferrer'>
           <FaLinkedin/>
        </a>
-       {/* <a href='https://www.youtube.com/channel/UCyORLTkS5hJmMQleHhW4xug' target='_blank' rel='noreferrer'>
-          <Image src={YT} height={70} width={70}/>
-       </a>
-       <a href='https://www.linkedin.com/company/bungalows-peru/' target='_blank' rel='noreferrer'>
-          <Image src={LI} height={60} width={60}/>
-       </a> */}
-
   </footer>
 }
 
